cca12a9f92f16cd2fe18d9b6fdc337aeb8314f94
EC2 Default User <ec2-user@ip-172-31-32-81.ap-northeast-1.compute.internal>
2023-07-15 16:29:56 +0900

    デプロイ用の

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -7,6 +7,12 @@ echo "render-bulid.sh: start"
 echo "installing bundle"
 bundle install
 
+echo "installing JavaScript packages"
+yarn install
+
+echo "compiling webpacker assets"
+bundle exec rails webpacker:compile
+
 echo "precompiling assets"
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
